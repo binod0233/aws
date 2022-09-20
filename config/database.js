cd7346e1bb4cd74ffe48a0d9395a415e067b47e9
binod <binodsharma0233@gmail.com>
2022-09-20 16:02:03 +0545

    update plug in

--- config/database.js
+++ config/database.js
@@ -4,10 +4,24 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "cdrassessmenthelp"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "root"),
-      ssl: env.bool("DATABASE_SSL", false),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
     },
+    useNullAsDefault: true,
   },
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "cdrassessmenthelp"),
+//       user: env("DATABASE_USERNAME", "postgres"),
+//       password: env("DATABASE_PASSWORD", "root"),
+//       ssl: env.bool("DATABASE_SSL", false),
+//     },
+//   },
+// });
